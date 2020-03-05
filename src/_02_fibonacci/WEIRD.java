6ca2888aad49fa4a1632b40d36722226a45f9696
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-03-04 18:23:41 -0800

    eh

---
+++ src/_02_fibonacci/WEIRD.java
@@ -0,0 +1,15 @@
+package _02_fibonacci;
+
+public class WEIRD {
+public static void main(String[] args) {
+	int next=0;
+	int fl=0;
+	int sl=1;
+	for (int i = 0; i < 12; i++) {
+		System.out.println(fl);
+		next=fl+sl;
+		fl=sl;
+		sl=next;
+	}
+}
+}
